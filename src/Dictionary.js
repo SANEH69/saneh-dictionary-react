b06aebd0740b866575fff3c2500faa7e03e76dfe
SANEH69 <sanehthandoh02@gmail.com>
2025-05-04 19:52:59 +0200

    Removed the if/else statements in my Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,25 +1,19 @@
 import React, { useState } from "react";
 import Meaning from './Meaning';
-import Photos from './Photos';
 import axios from "axios";
 import './Dictionary.css';
 
 export default function Dictionary(props) {
     const [word, setWord] = useState(props.word);
-    const [results, setResults] = useState({ ready: false});
+    const [results, setResults] = useState(" ");
     const [image, setImage] = useState(" ")
 
     function showMeaning(response) {
-        setResults({
-            ready: true,
-            results: response.data,
-    });
+        setResults(response.data);
     }
 
     function showImage(response) {
-        setImage({
-            image: response.data.photos,
-        })
+        setImage(response.data.photos)
     }
 
     function displayWord(event) {
@@ -31,7 +25,7 @@ export default function Dictionary(props) {
         setWord(event.target.value);
     }
 
-    function search() {
+    
     let apiKey = "04d1784de2be03a1bd2o2db8tf6b23e4";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`
     axios.get(apiUrl).then(showMeaning);
@@ -39,7 +33,7 @@ export default function Dictionary(props) {
     let imageApiKey = "04d1784de2be03a1bd2o2db8tf6b23e4";
     let imageApiUrl = `https://api.shecodes.io/images/v1/search?query=${word}&key=${imageApiKey}`
     axios.get(imageApiUrl).then(showImage);
-    }
+    
 
     let form = (
         <form onSubmit={displayWord}>
@@ -47,18 +41,14 @@ export default function Dictionary(props) {
         </form>
     )
 
-    if (results.ready) {
+    
     return (
         <div>
             {form}
             <div className="results">
             <h3>{word} | <em>{results.phonetic}</em></h3>
             <Meaning results={results} />
-            <Photos image={image} />
             </div>
         </div>
-    )} else {
-        search();
-        return <p>Fetching data...</p>
-    }
+    )
 }
--- src/Photos.js
+++ src/Photos.js
@@ -4,10 +4,10 @@ export default function Photos(props) {
     if (props.image) {
     return (
         <div>
-            <img src={props.image.src.original} alt={props.image.alt} />
+            <img src={props.image.src} alt={props.image.alt} />
         </div>
     );
-} else {
-    return <p>Generating image...</p>
-}
+    } else {
+        return <p>Generating image...</p>
+   }
 }
