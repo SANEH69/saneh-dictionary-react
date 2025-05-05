a484e8bc8c9d797c8b2ecf78c408ed9cbc07fa94
SANEH69 <sanehthandoh02@gmail.com>
2025-05-05 17:43:30 +0200

    Made my generated images display prettier using CSS

--- src/Photos.js
+++ src/Photos.js
@@ -1,9 +1,10 @@
 import React from "react";
+import './Photos.css'
 
 export default function Photos({ image }) {
     if (image && Array.isArray(image.photos)) {
         return (
-            <div>
+            <div className="images">
                 {image.photos.map((photo) => (
                     <div key={photo.id}>
                         <img src={photo.src.landscape} alt={image.alt} />
